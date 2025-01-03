f6b761518ed6e6e81285a3f3f3d83b6aa803dccb
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-01-03 23:29:40 +0900

    screen size and colours

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
-# This will be the main file for the self-driving simulation in Python
-# pip install neat-python
+# Self-Driving Simulation in Python
+import pygame
+import random
+import neat
+import os
+
+# Initialize pygame
+pygame.init()
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+GREEN = (0, 255, 0)
