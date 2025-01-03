fc88a11554b510497a7baf0def308a72de469179
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-01-03 23:32:46 +0900

    pygame display

--- main.py
+++ main.py
@@ -14,3 +14,12 @@ WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
 GREEN = (0, 255, 0)
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Self-Driving Car Simulation with NEAT")
+
+clock = pygame.time.Clock()
+
+maps = [
+    pygame.image.load(f"map{i}.png").convert() for i in range(1, 6)
+]
